80f77a72e4bd7a401e822c0db2bf34ba656b9c73
Ian Wulf <ianwulf24@gmail.com>
2025-10-29 13:01:53 -0500

    About Page -Added some framework elements to About page

--- src/about.js
+++ src/about.js
@@ -4,8 +4,23 @@ function displayAbout() {
     const contentContainer = document.createElement('div');
     contentContainer.classList.add('contentContainer');
     
-    content.appendChild(contentContainer); 
+    const aboutSection = document.createElement('div');
+    aboutSection.classList.add('about-section');
+
+    const aboutText = document.createElement('p');
+    aboutText.classList.add('about-text');
+
+    aboutSection.appendChild(aboutText);
+    contentContainer.appendChild(aboutSection);
+
+    const contactSection = document.createElement('div');
+    contactSection.classList.add('contact-section');
+
     
+    contentContainer.appendChild(contactSection);
+
+
+    content.appendChild(contentContainer); 
 
 }
 
